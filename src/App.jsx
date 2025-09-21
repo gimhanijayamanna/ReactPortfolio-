8ad35b18ab293de7cfc4dbde137521cf88a6c0d3
gimhanijayamanna <pabasarajayamanna@gmail.com>
2025-09-21 16:16:18 +0530

    hero page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
     return <div>
-        <section id="Home"><Navbar /></section>
+        <section id="Home"><Navbar /><Hero /></section>
         <section id="About">About</section>
         <section id="Skills">Skills</section>
         <section id="Projects">Projects</section>
